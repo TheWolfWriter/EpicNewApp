232901e4763a802030d1b00c67c2d7ac4e69779f
S. McKell Nichols <mckellnichols@hotmail.com>
2014-10-20 13:34:33 -0600

    Added to repo

---
+++ gen/ctec/epicnewapp/controller/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ctec.epicnewapp.controller;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/ctec/epicnewapp/controller/EpicActivity.java
@@ -0,0 +1,93 @@
+package ctec.epicnewapp.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class EpicActivity extends Activity
+{
+	public Button appFancyShmancyBlueButton;
+	public Button appMellowYellowButton;
+	public Button appBloodRedButton;
+	public Button appWickedGreenButton;
+	public Button appNothingRhymesWithThisOrangeButton;
+	public Button appPoppingPurpleButton;
+	private RelativeLayout appLayout;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_epic);
+		
+		appFancyShmancyBlueButton = (Button) findViewById(R.id.fancyShmancyBlueButton);
+		appMellowYellowButton = (Button) findViewById(R.id.mellowYellowButton);
+		appBloodRedButton = (Button) findViewById(R.id.bloodRedButton);
+		appWickedGreenButton = (Button) findViewById(R.id.wickedGreenButton);
+		appNothingRhymesWithThisOrangeButton = (Button) findViewById(R.id.nothingRhymesWithThisOrangeButton);
+		appPoppingPurpleButton = (Button) findViewById(R.id.poppingPurpleButton);
+		
+		setupListeners();
+	}
+	
+	private void setupListeners()
+	{
+		appFancyShmancyBlueButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				appLayout.setBackgroundResource(R.color.fancyShmancyBlue);
+			}
+		});
+		
+		appMellowYellowButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				appLayout.setBackgroundResource(R.color.mellowYellow);
+			}
+		});
+		
+		appBloodRedButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				appLayout.setBackgroundResource(R.color.bloodRed);
+			}
+		});
+		
+		appWickedGreenButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				appLayout.setBackgroundResource(R.color.wickedGreen);
+			}
+		});
+		
+		appNothingRhymesWithThisOrangeButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				appLayout.setBackgroundResource(R.color.nothingRhymesWithThisOrange);
+			}
+		});
+		
+		appPoppingPurpleButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				appLayout.setBackgroundResource(R.color.poppingPurple);
+			}
+		});
+	}
+}
